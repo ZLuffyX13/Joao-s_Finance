bfb73201fe5afc1803e5bdd02f803ca3bc093190
João André <joaoandre.fp@gmail.com>
2026-05-11 10:36:04 -0300

    Configurações iniciais de rotas e criação do projeto Angular e primeiros Components.

---
+++ finance/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Components
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+
+const routes: Routes = [
+
+  {path: '', component: LoginComponent},
+  {path: 'cadastro', component: CadastroComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ finance/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Components
+import { LoginComponent } from './components/login/login.component';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
